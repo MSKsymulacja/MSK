706c8b92dd98bc21f9e42408b9bc06ac21367033
Andrzej111 <pawel_szulc@onet.pl>
2014-03-24 23:07:39 +0100

    Update msk_mod.m
    Teraz działa sinus

--- msk_mod.m
+++ msk_mod.m
@@ -4,47 +4,47 @@ function [] = msk_mod(bity)
 ndt=100;
 t=0:1/ndt:1-1/ndt;
 syg=zeros(1,ndt*length(bity));
-if bity(1)==0
-    syg(1:ndt)=sin(2*pi*4/5*t);
-end
-
 if bity(1)==1
-    syg(1:ndt)=sin(2*pi*4/3*t);
+    syg(1:ndt)=sin(2*pi*5/4*t);
 end
 
-for i=2:length(bity)   
+if bity(1)==0
+    syg(1:ndt)=sin(2*pi*3/4*t);
+end
+
+for i=2:length(bity)
     if round(syg((i-1)*ndt))==0
         if syg((i-1)*ndt-1)<0
-            if bity(i)==0
-            syg((i-1)*ndt+1:i*ndt)=sin(2*pi*4/5*t);
-            else syg((i-1)*ndt+1:i*ndt)=sin(2*pi*4/3*t);
+            if bity(i)==1
+            syg((i-1)*ndt+1:i*ndt)=sin(2*pi*5/4*t);
+            else syg((i-1)*ndt+1:i*ndt)=sin(2*pi*3/4*t);
             end
         else
-            if bity(i)==0
-            syg((i-1)*ndt+1:i*ndt)=-sin(2*pi*4/5*t);
-            else syg((i-1)*ndt+1:i*ndt)=-sin(2*pi*4/3*t);
+            if bity(i)==1
+            syg((i-1)*ndt+1:i*ndt)=-sin(2*pi*5/4*t);
+            else syg((i-1)*ndt+1:i*ndt)=-sin(2*pi*3/4*t);
             end
         end
     end
     
     if round(syg((i-1)*ndt))==1
-            if bity(i)==0
-            syg((i-1)*ndt+1:i*ndt)=cos(2*pi*4/5*t);
-            else syg((i-1)*ndt+1:i*ndt)=cos(2*pi*4/3*t);
+            if bity(i)==1
+            syg((i-1)*ndt+1:i*ndt)=cos(2*pi*5/4*t);
+            else syg((i-1)*ndt+1:i*ndt)=cos(2*pi*3/4*t);
             end
-    end    
+    end
     
     if round(syg((i-1)*ndt))==-1
-            if bity(i)==0
-            syg((i-1)*ndt+1:i*ndt)=-cos(2*pi*4/5*t);
-            else syg((i-1)*ndt+1:i*ndt)=-cos(2*pi*4/3*t);
+            if bity(i)==1
+            syg((i-1)*ndt+1:i*ndt)=-cos(2*pi*5/4*t);
+            else syg((i-1)*ndt+1:i*ndt)=-cos(2*pi*3/4*t);
             end
-    end  
+    end
     
-    
-end    
-
-plot(syg);
     
 end
-
+T=0:length(bity)*ndt-1;
+T=T/100;
+plot(T,syg);
+    
+end
