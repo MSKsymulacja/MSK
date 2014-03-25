3cfd2de8d04357c7155542aef3ca134fdb40788f
Sniechu <p.sniechowski@gmail.com>
2014-03-25 17:47:19 +0100

    Dodanie treści do funkcji main
    Funkcja main docelowo ma być szkieletem programu. Zawiera odwołanie do funkcji modulator oraz dodanie szumu awgn, w przyszłości konieczna będzie implementacja demodulatora.

--- main_msk.m
+++ main_msk.m
@@ -1,21 +1,23 @@
+function [] = main(ilosc_bitow)
+% Ilość bitów, które chcemy przemodulować
 % I - tworzenie ciągu bitów 
-dlugosc=10;
-b= randi(2,1,dlugosc) - 1;      % generowanie słowa o zadanej długości  
+b= randi(2,1,ilosc_bitow) - 1;      % generowanie słowa o zadanej długości  
 
 subplot(3,1,1)
-stairs(b);
-axis([1 dlugosc -0.2 1.2]) 
+stairs(b,'LineWidth',3);
+axis([1 ilosc_bitow -0.2 1.2]) 
 
 % II - modulacja MSK
 ndt=100;                        % ilość próbkowań w czasie trwania jednego bitu
-sygnal=msk_mod(b,ndt); 
+sygnal=modulator(b,ndt); 
 
 subplot(3,1,2)
-plot(0:dlugosc*ndt-1,sygnal)    % przedstawienie zmodulowanego sygnału
+plot(0:ilosc_bitow*ndt-1,sygnal)    % przedstawienie zmodulowanego sygnału
 
 % III - przejście przez kanał AWGN
 snr=3;
 syg_szum=awgn(sygnal,snr);
 
 subplot(3,1,3)
-plot(0:dlugosc*ndt-1,syg_szum)  % przedstawienie zaszumionego sygnału
+plot(0:ilosc_bitow*ndt-1,syg_szum)  % przedstawienie zaszumionego sygnału
+end
