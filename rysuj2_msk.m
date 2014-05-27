a6e06da83a410d969063d6b311ff19a8ac4d275e
Sniechu <p.sniechowski@gmail.com>
2014-05-27 22:42:34 +0200

    Poprawa pliku rysuj2_msk
    Dodałem zmienną liczba_bitów, bo bez niej nie działało (przynajmniej u
    mnie)
    
    Dodałem bit wychodzący poza długość podanego słowa bitowego, który jest
    taki sam jak bit ostatni w tym słowie, dzięki temu można narysować
    wykres poprawnie (schodki) - w tym celu użyta zmienna "zmienna"
    
    Nie poprawiałem jednak innych wykresów - do poprawki

--- rysuj2_msk.m
+++ rysuj2_msk.m
@@ -1,18 +1,33 @@
 function [ output_args ] = rysuj2_msk( slowo_bitowe,snr )
 ndt=100;                                   % ilosc probkowan w czasie trwania jednego bitu
 
+%doda³em bit wychodz¹cy poza d³ugoœæ podanego s³owa bitowego, który jest
+%taki sam jak bit ostatni w tym s³owie, dziêki temu mo¿na narysowaæ wykres
+%poprawnie (schodki)
+
+%nie poprawia³em jednak innych wykresów - do poprawki
+
 %slowo_bitowe=randi(2,1,ilosc_bitow) - 1;
-
+ilosc_bitow=length(slowo_bitowe);
 sygnal=modulator_msk(slowo_bitowe,ndt); 
-
+zmienna=zeros(1,length(slowo_bitowe)+1);
+for i=1:length(slowo_bitowe)
+    zmienna(i)=slowo_bitowe(i);
+end
 syg_szum=awgn(sygnal,snr);
 
 zdemodulowane_bity=demodulator_msk(syg_szum,ilosc_bitow);
 
+if(slowo_bitowe(length(slowo_bitowe))==1);
+   zmienna(length(zmienna))=1;
+else
+   zmienna(length(zmienna))=0;
+end
+
 figure(2)
     subplot(4,1,1)
-    stairs(slowo_bitowe)
-    axis([1 ilosc_bitow -0.2 1.2]) 
+    stairs(zmienna)
+    axis([1 ilosc_bitow+1 -0.2 1.2]) 
     subplot(4,1,2)
     plot(0:1/ndt:ilosc_bitow-1/ndt,sygnal)
     subplot(4,1,3)
