c173f4b7ee45c4bf132d571b4a95c8b293833951
Paweł Szulc <pawel_szulc@onet.pl>
2014-06-19 18:49:03 +0200

    Rysowanie kompatybilne z nowym modulatorem/demodulatorem

--- rysuj2_msk.m
+++ rysuj2_msk.m
@@ -1,35 +1,48 @@
 function [ output_args ] = rysuj2_msk( slowo_bitowe,snr )
-ndt=100;                                   % ilosc probkowan w czasie trwania jednego bitu
-
-%doda³em bit wychodz¹cy poza d³ugoœæ podanego s³owa bitowego, który jest
-%taki sam jak bit ostatni w tym s³owie, dziêki temu mo¿na narysowaæ wykres
-%poprawnie (schodki)
-
-%nie poprawia³em jednak innych wykresów - do poprawki
-
-%slowo_bitowe=randi(2,1,ilosc_bitow) - 1;
+E=1;
+Tb=1;
+ndt=100;                                    % ilosc probkowan w czasie trwania jednego bitu
+fs=ndt/Tb;
 ilosc_bitow=length(slowo_bitowe);
-sygnal=modulator_msk(slowo_bitowe,ndt); 
-syg_szum=awgn(sygnal,snr);
-zdemodulowane_bity=demodulator_msk(syg_szum,ilosc_bitow);
+part_size=ilosc_bitow; % podzial na czesci aby nie obciazal tak pamieci przy wiekszej ilosci bitow
+
+    % dolozenie "sztucznych" bitow
+nieparzyste=mod(part_size,2);
+p=randi(2,1,part_size+2+nieparzyste) - 1;
+%slowo_bitowe=randi(2,1,part_size+2+nieparzyste) - 1;    % slowo bitowe wygenerowane losowo 
+p(2:end-1-nieparzyste)=slowo_bitowe;
+slowo_bitowe=p;
+faktyczne_slowo_bitowe=slowo_bitowe(2:end-1-nieparzyste);
+% modulator
+sygnal=sqrt(2*E/Tb)*modulator_msk(slowo_bitowe,ndt); 
+%kana³
+N=fs*10^( (-snr)/10);
+sigma=sqrt(N);
+szum=sigma*randn(1,(part_size+nieparzyste+1)*ndt);
+syg_szum=sygnal+szum;
+%demodulator
+zdemodulowane_bity=demodulator_msk(syg_szum,part_size+2+nieparzyste,fs,Tb);
+slowo_bitowe;
+zdemodulowane_bity;
+faktyczne_zdemodulowane_bity=zdemodulowane_bity(2:end-1-nieparzyste);
 
 st_b=zeros(1,ilosc_bitow+1);
-st_b(1:ilosc_bitow)=slowo_bitowe;
+st_b(1:ilosc_bitow)=faktyczne_slowo_bitowe;
 st_dem=zeros(1,ilosc_bitow+1);
-st_dem(1:ilosc_bitow)=zdemodulowane_bity;
-
+st_dem(1:ilosc_bitow)=faktyczne_zdemodulowane_bity;
 figure(2)
     subplot(4,1,1)
     stairs(0:ilosc_bitow,st_b)
     axis([0 ilosc_bitow -0.2 1.2]) 
     subplot(4,1,2)
-    plot(0:1/ndt:ilosc_bitow-1/ndt,sygnal)
+    plot(0:1/ndt:ilosc_bitow-1/ndt,sygnal(ndt+1:(ilosc_bitow+1)*ndt));
     subplot(4,1,3)
-    plot(0:1/ndt:ilosc_bitow-1/ndt,syg_szum)
+    plot(0:1/ndt:ilosc_bitow-1/ndt,syg_szum(ndt+1:(ilosc_bitow+1)*ndt));
     subplot(4,1,4)
     stairs(0:ilosc_bitow,st_dem)
     axis([0 ilosc_bitow -0.2 1.2]) 
 
 
 end
+
 
--- rysuj_msk.m
+++ rysuj_msk.m
@@ -1,25 +1,40 @@
 function [ output_args ] = rysuj_msk( ilosc_bitow,snr )
+E=1;
+Tb=1;
 ndt=100;                                    % ilosc probkowan w czasie trwania jednego bitu
-%ilosc_bitow=ilosc_bitow+1;
-slowo_bitowe=randi(2,1,ilosc_bitow) - 1;
+fs=ndt/Tb;
+part_size=ilosc_bitow; % podzial na czesci aby nie obciazal tak pamieci przy wiekszej ilosci bitow
 
-sygnal=modulator_msk(slowo_bitowe,ndt); 
+    % dolozenie "sztucznych" bitow
+nieparzyste=mod(part_size,2);
+slowo_bitowe=randi(2,1,part_size+2+nieparzyste) - 1;    % slowo bitowe wygenerowane losowo 
 
-syg_szum=awgn(sygnal,snr);
+faktyczne_slowo_bitowe=slowo_bitowe(2:end-1-nieparzyste);
+% modulator
+sygnal=sqrt(2*E/Tb)*modulator_msk(slowo_bitowe,ndt); 
+%kana³
+N=fs*10^( (-snr)/10);
+sigma=sqrt(N);
+szum=sigma*randn(1,(part_size+nieparzyste+1)*ndt);
+syg_szum=sygnal+szum;
+%demodulator
+zdemodulowane_bity=demodulator_msk(syg_szum,part_size+2+nieparzyste,fs,Tb);
+slowo_bitowe;
+zdemodulowane_bity;
+faktyczne_zdemodulowane_bity=zdemodulowane_bity(2:end-1-nieparzyste);
 
-zdemodulowane_bity=demodulator_msk(syg_szum,ilosc_bitow);
 st_b=zeros(1,ilosc_bitow+1);
-st_b(1:ilosc_bitow)=slowo_bitowe;
+st_b(1:ilosc_bitow)=faktyczne_slowo_bitowe;
 st_dem=zeros(1,ilosc_bitow+1);
-st_dem(1:ilosc_bitow)=zdemodulowane_bity;
+st_dem(1:ilosc_bitow)=faktyczne_zdemodulowane_bity;
 figure(2)
     subplot(4,1,1)
     stairs(0:ilosc_bitow,st_b)
     axis([0 ilosc_bitow -0.2 1.2]) 
     subplot(4,1,2)
-    plot(0:1/ndt:ilosc_bitow-1/ndt,sygnal)
+    plot(0:1/ndt:ilosc_bitow-1/ndt,sygnal(ndt+1:(ilosc_bitow+1)*ndt));
     subplot(4,1,3)
-    plot(0:1/ndt:ilosc_bitow-1/ndt,syg_szum)
+    plot(0:1/ndt:ilosc_bitow-1/ndt,syg_szum(ndt+1:(ilosc_bitow+1)*ndt));
     subplot(4,1,4)
     stairs(0:ilosc_bitow,st_dem)
     axis([0 ilosc_bitow -0.2 1.2]) 
