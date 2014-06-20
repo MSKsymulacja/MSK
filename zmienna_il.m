6be18d72137a588e07fec8adc595aa483a368af2
Paweł Szulc <pawel_szulc@onet.pl>
2014-06-20 23:34:09 +0200

    Zmienna ilość bitów
    Dodanie funkcji, która odpowiada za symulację transmisji zmiennej ilości
    bitów do osiągnięcia zadanej szerokości przedziałów wilsona

---
+++ zmienna_il.m
@@ -0,0 +1,18 @@
+function [ ilosc ] = zmienna_il( szer,snr,alfa )
+        n=10;
+        suma=0;
+        il_b=0;
+        up_down=1;
+        while (up_down>szer)
+            ber=glowny_msk(n,snr);
+            il=ber*n;
+            il_b=il_b+il;
+            suma=suma+n;
+            [d,u]=wilson_conf(il_b,suma,alfa);
+            up_down=u-d;
+            n=10^floor(log10(suma)); % jesli bylo 100 to n=100 jak dojdzie do 1000 to n=1000 itd.
+            end
+ilosc=suma;
+
+end
+
