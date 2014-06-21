1e0bd4c2edfd3f024cc608789d36ef6f91fe6848
Paweł Szulc <pawel_szulc@onet.pl>
2014-06-21 14:15:52 +0200

    Implementacja funkcjonalności do intefejsu
    Dodanie poprawnego wyświetlania wyników symulacji o zmiennej ilości
    bitów

--- gui.m
+++ gui.m
@@ -143,6 +143,7 @@ przedzial=str2num(get(handles.edit9,'String'));
 snrx5=str2num(get(handles.edit10,'String'));
 snr5=snrx5(1):snrx5(length(snrx5));
 wilson5=str2num(get(handles.edit11,'String'));
-t=zmienna_il(przedzial,snr5,wilson5/100) 
-t
+[t,ber,up,down]=zmienna_il(przedzial,snr5,wilson5/100);
+przedzial=cat(2,num2str(down),' - ',num2str(up));
+h=msgbox(cat(2,{'Wymagana liczba powtórzeñ: '},num2str(t),{'Zmierzony BER: '},num2str(ber),{'Przedzia³ Wilsona dla zadanych parametrów: '},przedzial),'Obliczono');
 end
--- zmienna_il.m
+++ zmienna_il.m
@@ -1,4 +1,4 @@
-function [ ilosc ] = zmienna_il( szer,snr,alfa )
+function [ ilosc,ber_koncowy,gorny_przedzial,dolny_przedzial ] = zmienna_il( szer,snr,alfa )
         n=10;
         suma=0;
         il_b=0;
@@ -11,8 +11,10 @@ function [ ilosc ] = zmienna_il( szer,snr,alfa )
             [d,u]=wilson_conf(il_b,suma,alfa);
             up_down=u-d;
             n=10^floor(log10(suma)); % jesli bylo 100 to n=100 jak dojdzie do 1000 to n=1000 itd.
-            end
+        end
 ilosc=suma;
-
+ber_koncowy=il_b/ilosc;
+gorny_przedzial=u;
+dolny_przedzial=d;
 end
 
